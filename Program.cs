789d7a98af5f76b1e6a0040449d70578fecf9bc0
GabrielLareglia <laregliagabriel@gmail.com>
2023-04-02 18:34:55 -0300

    terminei

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nome, sobrenome;
+Console.Write("coloque seu nome.......: ");
+nome = Console.ReadLine()!;
+
+Console.Write("coloque seu sobrenome..: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
